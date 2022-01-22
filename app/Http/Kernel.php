dd1f07038c294070838606a8a77e6f29bdf61155
Hwassef <hassine.wassef@outlook.com>
2022-01-21 15:26:54 -1100

    adding admin theme

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'theme' => \Qirolab\Theme\Middleware\ThemeMiddleware::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ let theme = process.env.npm_config_theme;
+
+ if(theme) {
+    require(`${__dirname}/themes/${theme}/webpack.mix.js`);
+ } else {
+     // default theme to compile if theme is not specified
+   require(`${__dirname}/themes/theme-name/webpack.mix.js`);
+ }
