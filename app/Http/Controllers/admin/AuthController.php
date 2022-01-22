d6056b13e5a7383e8e105a7e2eebb917444a1a3f
Hwassef <hassine.wassef@outlook.com>
2022-01-21 15:53:03 -1100

    adding login and logout routes

--- app/Http/Controllers/admin/AuthController.php
+++ app/Http/Controllers/admin/AuthController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        if (!Auth::guard('admin')->attempt($request->only('email', 'password'))) {
+            throw ValidationException::withMessages([
+                'email' => 'Invalid Email or Password'
+            ]);
+        }
+
+        return redirect()->intended(route('admin.home',));
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect('/');
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->name('admin.')->middleware('theme:admin')->group(function(){
     Route::View('/login', 'auth.login')->name('login');
+
 });
