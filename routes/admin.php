d42969d9f27205d9d885eb2ae6c9449d274a497b
Hwassef <hassine.wassef@outlook.com>
2022-01-21 15:54:02 -1100

    adding login and logout routes

--- routes/admin.php
+++ routes/admin.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->name('admin.')->middleware('theme:admin')->group(function(){
     Route::View('/login', 'auth.login')->name('login');
-
+    Route::post('/login', [App\Http\Controllers\Admin\AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [App\Http\Controllers\Admin\AuthController::class, 'logout'])->name('logout');
 });
