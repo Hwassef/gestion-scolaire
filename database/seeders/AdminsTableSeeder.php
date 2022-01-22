7b3e5a82e78f3f20ca5e0a1690fd7571b57b06ea
Hwassef <hassine.wassef@outlook.com>
2022-01-21 15:57:01 -1100

    adding admin seeder

---
+++ database/seeders/AdminsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'full_name' => "Wassef Hassine",
+            'email' => "hassinewassef@gmail.com",
+            'password' => bcrypt('hw')
+        ]);
+    }
+}
